dc4a9bb01556e8822aa4017d446a08ba9022cda4
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-11 17:49:57 +0000

    simplified routes to use resources:

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,42 @@
 Rails.application.routes.draw do
   root "movies#index" 
+  resources:movies
+  resources:directors
+  # # Routes for the Movie resource:
 
-  # Routes for the Movie resource:
-
-  # CREATE
-  post "/movies" =>  "movies#create", as: :movies
-  get "/movies/new" =>  "movies#new", as: :new_movie
+  # # CREATE
+  # post "/movies" =>  "movies#create", as: :movies
+  # get "/movies/new" =>  "movies#new", as: :new_movie
           
-  # READ
-  get "/movies" =>  "movies#index" 
-  get "/movies/:id" =>  "movies#show", as: :movie
+  # # READ
+  # get "/movies" =>  "movies#index" 
+  # get "/movies/:id" =>  "movies#show", as: :movie
   
-  # UPDATE
-  patch "/movies/:id" =>  "movies#update"
-  get "/movies/:id/edit" =>  "movies#edit", as: :edit_movie
+  # # UPDATE
+  # patch "/movies/:id" =>  "movies#update"
+  # get "/movies/:id/edit" =>  "movies#edit", as: :edit_movie
   
-  # DELETE
-  delete "/movies/:id" =>  "movies#destroy" 
+  # # DELETE
+  # delete "/movies/:id" =>  "movies#destroy" 
 
-  #------------------------------
+  # #------------------------------
 
-  # Routes for the directors
+  # # Routes for the directors
 
-    # CREATE
-    post "/directors" =>  "directors#create", as: :directors
-    get "/directors/new" =>  "directors#new", as: :new_director
+  #   # CREATE
+  #   post "/directors" =>  "directors#create", as: :directors
+  #   get "/directors/new" =>  "directors#new", as: :new_director
             
-    # READ
-    get "/directors" =>  "directors#index" 
-    get "/directors/:id" =>  "directors#show", as: :director
+  #   # READ
+  #   get "/directors" =>  "directors#index" 
+  #   get "/directors/:id" =>  "directors#show", as: :director
     
-    # UPDATE
-    patch "/directors/:id" =>  "directors#update"
-    get "/directors/:id/edit" =>  "directors#edit", as: :edit_director
+  #   # UPDATE
+  #   patch "/directors/:id" =>  "directors#update"
+  #   get "/directors/:id/edit" =>  "directors#edit", as: :edit_director
     
-    # DELETE
-    delete "/directors/:id" =>  "directors#destroy" 
+  #   # DELETE
+  #   delete "/directors/:id" =>  "directors#destroy" 
   
     #------------------------------
 end
